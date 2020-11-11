535806682863cbaec86824e212c6acd7bdd3829c
Chetan2028 <bmchetan2028@gmail.com>
2020-11-11 22:22:55 +0530

    [Chetan] Add . Added to find the cheapest hotel

--- HotelReservationSystem/HotelReservation.cs
+++ HotelReservationSystem/HotelReservation.cs
@@ -6,13 +6,16 @@ namespace HotelReservationSystem
 {
     class HotelReservation
     {
+
+        List<Hotel> hotelList = new List<Hotel>();
+        SortedDictionary<int,string> ratesAndHotelsDictionary = new SortedDictionary<int,string>();
+
         /// <summary>
         /// UC1
         /// Adds the hotels.
         /// </summary>
         public void AddHotels()
         {
-            List<Hotel> hotelList = new List<Hotel>();
             hotelList.Add(new Hotel("Lakewood", 110));
             hotelList.Add(new Hotel("Bridgewood", 160));
             hotelList.Add(new Hotel("Ridgewood", 220));
@@ -21,6 +24,50 @@ namespace HotelReservationSystem
             foreach (Hotel hotels in hotelList)
             {
                 Console.WriteLine(hotels.hotelName + "      :    " + hotels.weekDayRegularRates);
+            }
+        }
+
+        /// <summary>
+        /// UC2
+        /// Calculatings the hotel prices.
+        /// </summary>
+        /// <param name="checkInDate">The check in date.</param>
+        /// <param name="checkOutDate">The check out date.</param>
+        public void CalculatingHotelPrices(DateTime checkInDate , DateTime checkOutDate)
+        {
+            if (checkInDate < checkOutDate)
+            {
+                TimeSpan diff = checkOutDate.Subtract(checkInDate);
+                foreach (Hotel hotels in hotelList)
+                {
+                    int total = 0;
+                    for(int i=0; i<diff.TotalDays; i++)
+                    {
+                        total = total +  hotels.weekDayRegularRates;
+                    }
+                    Console.WriteLine("Hotel Name : {0} and Total Price : {1}",hotels.hotelName,total);
+                    ratesAndHotelsDictionary.Add(total, hotels.hotelName);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter dates properly");
+            }
+        }
+
+        /// <summary>
+        /// UC2
+        /// Findings the cheapest hotel.
+        /// </summary>
+        /// <param name="checkInDate">The check in date.</param>
+        /// <param name="checkOutDate">The check out date.</param>
+        public void FindingCheapestHotel(DateTime checkInDate , DateTime checkOutDate)
+        {
+            CalculatingHotelPrices(checkInDate, checkOutDate);
+            foreach (var hotelPrice in ratesAndHotelsDictionary)
+            {
+                Console.WriteLine("Cheapest Hotel Name : {0} , Total Price : {1}",hotelPrice.Value,hotelPrice.Key);
+                break;
             }
         }
     }
--- HotelReservationSystem/Program.cs
+++ HotelReservationSystem/Program.cs
@@ -9,6 +9,15 @@ namespace HotelReservationSystem
             Console.WriteLine("Welcome to Hotel Reservation System");
             HotelReservation hotelReservation = new HotelReservation();
             hotelReservation.AddHotels();
+            Console.WriteLine("Enter the check-in date in the form dd/mm/yyyy");
+            string checkIn = Console.ReadLine();
+            Console.WriteLine("Enter the checkout date in the form dd/mm/yyyy");
+            string checkOut = Console.ReadLine();
+            DateTime checkInDate = DateTime.Parse(checkIn);
+            Console.WriteLine(checkInDate.ToString("d"));
+            DateTime checkOutDate = DateTime.Parse(checkOut);
+            Console.WriteLine(checkOutDate.ToString("d"));
+            hotelReservation.FindingCheapestHotel(checkInDate, checkOutDate);
         }
     }
 }
