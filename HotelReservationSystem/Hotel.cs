fffe30be0316e6cbe51672a5a8f561bdd4b05b96
Chetan2028 <bmchetan2028@gmail.com>
2020-11-12 13:51:35 +0530

    [Chetan] Add . Added ratings for each hotel

--- HotelReservationSystem/Hotel.cs
+++ HotelReservationSystem/Hotel.cs
@@ -10,6 +10,7 @@ namespace HotelReservationSystem
         public string hotelName;
         public int weekDayRegularRates;
         public int weekendRegularRates;
+        public int hotelRatings;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Hotel"/> class.
@@ -21,6 +22,21 @@ namespace HotelReservationSystem
             this.hotelName = hotelName;
             this.weekDayRegularRates = weekDayRegularRates;
             this.weekendRegularRates = weekendRegularRates;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Hotel"/> class.
+        /// </summary>
+        /// <param name="hotelName">Name of the hotel.</param>
+        /// <param name="weekDayRegularRates">The week day regular rates.</param>
+        /// <param name="weekendRegularRates">The weekend regular rates.</param>
+        /// <param name="hotelRatings">The hotel ratings.</param>
+        public Hotel(string hotelName , int weekDayRegularRates , int weekendRegularRates , int hotelRatings)
+        {
+            this.hotelName = hotelName;
+            this.weekDayRegularRates = weekDayRegularRates;
+            this.weekendRegularRates = weekendRegularRates;
+            this.hotelRatings = hotelRatings;
         }
     }
 }
